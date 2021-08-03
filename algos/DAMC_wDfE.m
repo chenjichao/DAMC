883a364c86b49a9c897a317e443a6cd0bf0dcb63
chenjichao <e160075@e.ntu.edu.sg>
2021-08-03 11:31:03 +0800

    [feat] ablation study only ELM.

--- algos/DAMC_wDfE.m
+++ algos/DAMC_wDfE.m
@@ -100,27 +100,31 @@ dist_e = distancefusion_within(temp);
 dist_de = zeros(nViw,nSmp,nSmp);
 % dist_d = dist_d * (1-rate_embed);
 % dist_e = dist_e * rate_embed;
-for iViw = 1:nViw
-    switch fusion3
-        case 'pd'
-            dist_de(iViw,:,:) = dist_d(iViw,:,:).*dist_e(iViw,:,:);
-        case 'gm'
-            if rate_embed == 0
-                dist_de(iViw,:,:) = dist_d(iViw,:,:);
-            elseif rate_embed == 1
-                dist_de(iViw,:,:) = dist_e(iViw,:,:);
-            else
-                dist_de(iViw,:,:) = sqrt(dist_d(iViw,:,:).*dist_e(iViw,:,:));
-            end
-        case 'sm'
-            dist_de(iViw,:,:) = dist_d(iViw,:,:) + dist_e(iViw,:,:);
-        case 'am'
-%                 dist_de(iViw,:,:) = (dist_d(iViw,:,:)+dist_e(iViw,:,:))/2;
-            dist_de(iViw,:,:) = dist_d(iViw,:,:) + dist_e(iViw,:,:);
-        otherwise
-            dist_de(iViw,:,:) = dist_d(iViw,:,:).*dist_e(iViw,:,:);
-    end
-end
+
+dist_de = dist_e;
+% for iViw = 1:nViw
+%     switch fusion3
+%         case 'pd'
+%             dist_de(iViw,:,:) = dist_d(iViw,:,:).*dist_e(iViw,:,:);
+%         case 'gm'
+%             if rate_embed == 0
+%                 dist_de(iViw,:,:) = dist_d(iViw,:,:);
+%             elseif rate_embed == 1
+%                 dist_de(iViw,:,:) = dist_e(iViw,:,:);
+%             else
+%                 dist_de(iViw,:,:) = sqrt(dist_d(iViw,:,:).*dist_e(iViw,:,:));
+%             end
+%         case 'sm'
+%             dist_de(iViw,:,:) = dist_d(iViw,:,:) + dist_e(iViw,:,:);
+%         case 'am'
+% %                 dist_de(iViw,:,:) = (dist_d(iViw,:,:)+dist_e(iViw,:,:))/2;
+%             dist_de(iViw,:,:) = dist_d(iViw,:,:) + dist_e(iViw,:,:);
+%         otherwise
+%             dist_de(iViw,:,:) = dist_d(iViw,:,:).*dist_e(iViw,:,:);
+%     end
+% end
+
+
 % lambdas = [];
 % objfun1s = [];
 %% repeat
